b451cfc6c34a6d372ae280ceaad823a2dfe7e128
EbrahimOseif <ebrahimoseif@gmail.com>
2018-04-28 21:24:18 +0200

    FEE Community App is ready now to be tested
    +++++++++ Completed
    camera fragment
    
    +++++++++ to be continued
    search Activity using auto complete text view
    messages fragment
    
    ++++++++++ Later
    following and followers
    layout colors and design

--- app/src/main/java/ebrahimoseif/com/FeeCommunity/Home/CameraFragment.java
+++ app/src/main/java/ebrahimoseif/com/FeeCommunity/Home/CameraFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ebrahimoseif.com.FeeCommunity.Profile.AccountSettingsActivity;
 import ebrahimoseif.com.FeeCommunity.R;
@@ -26,17 +27,57 @@ import ebrahimoseif.com.FeeCommunity.Utils.Permissions;
 
 public class CameraFragment extends Fragment {
     private static final String TAG = "CameraFragment";
-
-    private static final int  CAMERA_REQUEST_CODE = 5;
-
-
+    private Button button1;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
+        button1 = (Button)  view.findViewById(R.id.btnLaunchCamera);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 5);
+            }
+        });
 
         return view;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+// CAMERA_REQUEST_CODE = 5
+        if(requestCode == 5 ){
+            Log.d(TAG, "onActivityResult: done taking a photo.");
+            Log.d(TAG, "onActivityResult: attempting to navigate to final share screen.");
+
+            Bitmap bitmap;
+            bitmap = (Bitmap) data.getExtras().get("data");
+
+            if(  data.getFlags() == 0 ){
+                try{
+                    Log.d(TAG, "onActivityResult: received new bitmap from camera: " + bitmap);
+                    Intent intent = new Intent(getActivity(), NextActivity.class);
+                    intent.putExtra(getString(R.string.selected_bitmap), bitmap);
+                    startActivity(intent);
+                }catch (NullPointerException e){
+                    Log.d(TAG, "onActivityResult: NullPointerException: " + e.getMessage());
+                }
+            }else{
+                try{
+                    Log.d(TAG, "onActivityResult: received new bitmap from camera: " + bitmap);
+                    Intent intent = new Intent(getActivity(), AccountSettingsActivity.class);
+                    intent.putExtra(getString(R.string.selected_bitmap), bitmap);
+                    intent.putExtra(getString(R.string.return_to_fragment), getString(R.string.edit_profile_fragment));
+                    startActivity(intent);
+                    getActivity().finish();
+                }catch (NullPointerException e){
+                    Log.d(TAG, "onActivityResult: NullPointerException: " + e.getMessage());
+                }
+            }
+
+        }
     }
 
     }
